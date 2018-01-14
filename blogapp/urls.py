1f198102a477dcc73359eead73ce1ab5442840e5
kumarm5 <mklkumar330@gmail.com>
2018-01-14 20:59:22 +0530

    pagination in listing of blog

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,9 +4,11 @@ from .models import *
 
 urlpatterns = [
     url(r'^$', views.Home.as_view(template_name='index.html'), name='blog'),
+    url(r'^page(?P<num>[0-9]+)/$', views.Home.as_view(template_name='index.html'), name='blog_page'),
     url(r'^post/(?P<post_id>\d+)/$', views.Post.as_view(template_name='post.html'), name='post'),
     url(r'^about/', views.About.as_view(template_name='about.html'), name='about'),
     url(r'^contact/', views.Contact.as_view(template_name='contact.html'), name='contact'),
     url(r'^search/', views.Search.as_view(template_name='search.html'), name='search'),
     url(r'^tag/(?P<tag_id>\d+)/$', views.Tag.as_view(template_name='tags.html'), name='tags'),
+    url(r'^tag/(?P<tag_id>\d+)/(?P<num>[0-9]+)$', views.Tag.as_view(template_name='tags.html'), name='tag_page'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -2,16 +2,30 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import *
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import pdb
 
 # Create your views here.
 class Home(TemplateView):
-    def get(self, request, **kwargs):        
+    def get(self, request, *args, **kwargs):
         tagdetails = Tags.objects.all()
-        blogdetails = Blog.objects.all()
-
+        blogdetail_list = Blog.objects.all()
+        try:
+            page = int(kwargs['num'])
+        except:
+            page = None
+        
+        paginator = Paginator(blogdetail_list, 6)
+        try:
+            blogdetails = paginator.page(page)
+        except PageNotAnInteger:
+            blogdetails = paginator.page(1)
+        except EmptyPage:
+            blogdetails = paginator.page(paginator.num_pages)
+            
+        # get the latest blog details
         latestblog = Blog.objects.latest('id')
-
+        
         return render(request, self.template_name, {'blogdetails': blogdetails, 'tagdetails': tagdetails, 'latestblog': latestblog})
 
 class Post(TemplateView):
@@ -42,15 +56,31 @@ class Tag(TemplateView):
         tagdetails = Tags.objects.all()
         tag_id = int(kwargs['tag_id'])
 
+        # get the tag details
         try:
             tag_detail = Tags.objects.get(pk=tag_id)
         except:
             tag_detail = None
-
-        blogdetails = Blog.objects.filter(tag_id=tag_id)        
         
+        blogdetail_list = Blog.objects.filter(tag_id=tag_id)        
+        
+        try:
+            page = int(kwargs['num'])
+        except:
+            page = None
+        
+        # pagination
+        paginator = Paginator(blogdetail_list, 6)
+        try:
+            blogdetails = paginator.page(page)
+        except PageNotAnInteger:
+            blogdetails = paginator.page(1)
+        except EmptyPage:
+            blogdetails = paginator.page(paginator.num_pages)
+            
+        # latest blog
         latestblog = Blog.objects.latest('id')
-
+        
         return render(request, self.template_name, { 'tagdetails': tagdetails, 'blogdetails': blogdetails, 'tag_detail': tag_detail, 'latestblog': latestblog })
 
 class Search(TemplateView):
